4bdb3e3018087a3cd4513365b663887b5ff02c04
Amy Wong <akwong24@outlook.com>
2024-09-01 21:10:16 -0700

    Contact form works, will be filling in info

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import "./Contact.scss";
 import { motion } from "framer-motion";
+import emailjs from '@emailjs/browser';
 
 const variants = {
     initial: {
@@ -18,6 +19,27 @@ const variants = {
 }
 
 const Contact = () => {
+    const ref = useRef();
+    const [error, setError] = useState(false);
+    const [success, setSuccess] = useState(false);
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+    
+        emailjs
+          .sendForm('service_opqblrb', 'template_5lgl4k2', ref.current, {
+            publicKey: '2VRvrktIUrAbfMpEy',
+          })
+          .then(
+            () => {
+              setSuccess(true)
+            },
+            (error) => {
+              setError(true)
+            },
+          );
+      };
+
     return (
         <motion.div className="contact" variants={variants} initial="initial" whileInView="animate">
             <motion.div className="textContainer" variants={variants}>
@@ -32,11 +54,13 @@ const Contact = () => {
                 </motion.div>
             </motion.div>
             <div className="formContainer" variants={variants}>
-                <form>
-                    <input type="text" placeholder="Name" required />
-                    <input type="email" placeholder="Email Address" required />
-                    <textarea placeholder="Message" required rows={8}/>
+                <form ref={ref} onSubmit={sendEmail}>
+                    <input type="text" placeholder="Name" required name="from_name" />
+                    <input type="email" placeholder="Email Address" required name="email" />
+                    <textarea placeholder="Message" required rows={8} name="message" />
                     <button>Submit</button>
+                    {error && "Message not sent. Please check if you filled in all the boxes before trying again."}
+                    {success && "Message received! Expect a reply from me in 1-2 days."}
                 </form>
             </div>
         </motion.div>
