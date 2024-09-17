9af54e3211b9afa13554c5ca249ff052b51f919e
Amy Wong <akwong24@outlook.com>
2024-09-16 20:49:32 -0700

    mobile responsiveness

--- src/components/about.js
+++ src/components/about.js
@@ -27,25 +27,38 @@ const About = () => {
             </motion.div>
             <motion.div className="contentContainer" variants={variants}>
                 <div className="textContainer">
+                    <h4>Get to Know Me!</h4>
                     <p>My name is Amy and I'm a Chinese-American from the San Gabriel Valley in California.
-                        I recently graduated from Cal State LA in May 2024 with a Bachelor's degree in Computer 
-                        Information Systems, specializing in Information Technology. With a passion 
-                        for technology and a drive to innovate, I am actively seeking for new full-time graduate 
+                        I recently graduated from Cal State LA in May 2024 with a Bachelor's degree in Computer
+                        Information Systems, specializing in Information Technology. With a passion
+                        for technology and a drive to innovate, I am actively seeking for new full-time graduate
                         roles.</p>
-                        <br />
-                        <p>I have a keen interest towards Software Development (mainly Frontend) and
-                        Data Analysis. One of the projects I enjoyed working on is MuseVibe, which is
+                    <br />
+                    <p>I have a keen interest towards Software Development (mainly Frontend), Android Development,
+                        and Data Analysis. One of the projects I enjoyed working on is MuseVibe, which is
                         a frontend, Spotify-inspired music website where you can listen to music for
                         free. I do plan to continue with that project by learning and utilizing the MERN (MongoDB,
-                        ExpressJS, ReactJS, and NodeJS) stack to make the site fully functional. I enjoy 
+                        ExpressJS, ReactJS, and NodeJS) stack to make the site fully functional. I enjoy
                         creating applications and ensuring that it is user-friendly.</p>
-                        <br />
-                        <p>When I'm not in the technology world, I enjoy playing video games, learning a new
-                            language, and reading books and novels. I am currently learning Korean (한국어)
-                            out of interest. I also would like to learn Mandarin Chinese (中文) in the future.</p>
+                    <br />
+                    <p>When I'm not in the technology world, I enjoy playing video games, learning a new
+                        language, and reading books and novels. I am currently learning Korean (한국어)
+                        out of interest. I also would like to learn Mandarin Chinese (中文) in the future.</p>
                 </div>
-                <div className="imageContainer">
-                    <img src="/amy_la.jpg" alt="" />
+                <div className="skillsContainer">
+                <h4>My Skills</h4>
+                    <div className="skillsetContainer">
+                        <div className="skill">HTML/CSS</div>
+                        <div className="skill">JavaScript</div>
+                        <div className="skill">ReactJS</div>
+                        <div className="skill">Visual Studio Code</div>
+                        <div className="skill">Replit</div>
+                        <div className="skill">Github</div>
+                        <div className="skill">Git</div>
+                        <div className="skill">Microsoft Office (Word, Excel, PowerPoint)</div>
+                        <div className="skill">PowerBI</div>
+                        <div className="skill">Microsoft Project</div>
+                    </div>
                 </div>
             </motion.div>
         </motion.div>
